5dc668ce6f9370f84571402ea06c331f42f1df90
ruchudechunzhen <975903522@qq.com>
2026-04-26 19:39:13 +0800

    新增RAG模块

---
+++ ai-springai-RAG/src/main/java/com/example/aispringairag/AiSpringaiRagApplication.java
@@ -0,0 +1,13 @@
+package com.example.aispringairag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiSpringaiRagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiSpringaiRagApplication.class, args);
+    }
+
+}
---
+++ ai-springai-RAG/src/test/java/com/example/aispringairag/AiSpringaiRagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aispringairag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiSpringaiRagApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
